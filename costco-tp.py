95e41de63ebb95cf24b432c38016800ec290a5a4
Brenden Hyde <brenden.hyde@autodesk.com>
2020-03-26 16:05:04 -0700

    comment out the RIGHT line

--- costco-tp.py
+++ costco-tp.py
@@ -10,8 +10,8 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
-#from selenium.webdriver.support import expected_conditions as EC
+#from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 import sys
 import time
